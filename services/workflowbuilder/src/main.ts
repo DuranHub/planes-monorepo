6859e89762dd4a3f71f5e35f4540699b4c68e3cc
RamAyala0 <sairamses@gmail.com>
2023-10-03 12:31:27 -0600

    :adhesive_bandage: Fix some titles in the document builder of the swagger module

--- services/workflowbuilder/src/main.ts
+++ services/workflowbuilder/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   console.log(`⚡️ Running on http://localhost:3000`);
 
   const config = new DocumentBuilder()
-    .setTitle('Workflow Builder service')
+    .setTitle('Workflow Builder Services')
     .setDescription('Service dedicated to link data inputs to activities, and activities to entities, aimed to create a Procedure Project')
     .setVersion('0.0.1')
     .addTag('Workflow Builder Service')
