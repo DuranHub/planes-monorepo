b77fef7738b92888232ef115124c794707c15f2f
Daniel Payan <dany.sasket@gmail.com>
2023-10-09 17:39:47 -0600

    :lipstick: Add Integer input

--- packages/planes-ds/src/lib/components/atoms/input/Input.stories.tsx
+++ packages/planes-ds/src/lib/components/atoms/input/Input.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Input } from './index';
 import ComponentResolver from './componentResolver';
+import { validate } from './validate'
+import {validate_Input} from './validate'
+import { IntegerInput } from './integerInput';
 
 export default {
     title: "Input",
@@ -12,8 +15,12 @@ export default {
         }
     }
 }
+validate();
 
 const Template = (args) => <ComponentResolver {...args} />; //Arguments for component resolver
+
+const IntegerInputArgs = args => <IntegerInput {...args} />
+
 
 export const Text = Template.bind({});
 Text.args = {
@@ -23,4 +30,12 @@ Text.args = {
 export const Alphabetic = Template.bind({});
 Alphabetic.args = {
     inputType: 'alphabetic',
+};
+
+export const Integer_Second = IntegerInputArgs.bind({});
+Integer_Second.args = {
+    inputType: 'Integer',
+    //type: 'number',
+    placeholder: "123",
+    value: 1,
 };
