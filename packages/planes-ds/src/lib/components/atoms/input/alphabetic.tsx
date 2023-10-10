c1ab24744bccd4f328ae83b1efda1d30119639fa
shurrini <jesusrt2608@gmail.com>
2023-10-09 18:14:28 -0600

    Add alphabetic input component
    Added alphabetic input component, which uses zod for schema validation.

--- packages/planes-ds/src/lib/components/atoms/input/alphabetic.tsx
+++ packages/planes-ds/src/lib/components/atoms/input/alphabetic.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { ZodError, z } from "zod";
+import { useState } from "react";
 
 //import { cn } from "@/lib/utils"
 import { cn } from "../../../utils"
@@ -10,20 +12,42 @@ export interface InputProps
 
 const AlphabeticInput = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, type, isRequired, ...props }, ref) => {
+    
+    const [error, setError] = useState(null); //Hook to manage error state
+
+    // This function is called whenever the input changes
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Define the schema for alphabetic input, with a regular expression for both spanish and english characters
+        const schema = z.string().regex(/^[a-zA-Z0-9\s\p{P}áéíóúÁÉÍÓÚñÑ]+$/, "Input must be alphabetic");    
+        try {
+            schema.parse(e.target.value); //If parsing is succesful, set error state to null
+            setError(null);
+        } catch (error: any) {
+            setError(error.message); //If parsing fails, set error message
+        }
+    }
+
     return (
+        <div>
         <input
             type={type}
             required={isRequired}
             className={cn(
                 "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 invalid:bg-red-500",
-                className
+                className,
+                error && "bg-red-500"
             )}
             ref={ref}
+            onBlur={handleInputChange}
             {...props}
         />
+        {/* Show error message */}
+        {error && <p>{error}</p>}
+        </div>
     )
     }
 )
-AlphabeticInput.displayName = "Input"
+
+AlphabeticInput.displayName = "AlphabeticalInput"
 
 export { AlphabeticInput }
