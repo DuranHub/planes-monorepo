23188ecbdd034aec4973797ae73e83a2990afb53
shurrini <jesusrt2608@gmail.com>
2023-11-16 11:59:10 -0600

    update server-timings from previous branch

--- services/dashboard/tests/mocks/workflow.ts
+++ services/dashboard/tests/mocks/workflow.ts
@@ -1,25 +1,31 @@
 import { faker } from '@faker-js/faker'
-import { HttpResponse, http, type HttpHandler } from 'msw'
+import { HttpResponse, http, type HttpHandler} from 'msw'
+import {
+	makeTimings,
+	time,
+} from '#app/utils/timing.server.ts'
 
 const { json } = HttpResponse
 
 export const handlers: Array<HttpHandler> = [
-	http.get(`https://localhost:7777/workflow`, async () => {
-		console.info('🔶 mocking reactflow components:')
+    http.get(`https://localhost:7777/workflow`, async () => { 
+        console.info('🔶 mocking reactflow components:')
+        const timings = makeTimings('workflow loader') //setting up timings
 
-        const place_1 = faker.location.city();
-        const place_2 = faker.location.city();
+        const place_1 = faker.location.city()
+        const place_2 = faker.location.city()
 
-		return json({
-			"nodes": [
+        // Time functions
+        const nodes = await time(
+            () => [
                 {
                     "id": "button-1",
                     "data": { 
                         "label": place_1
                     },
                     "position": { "x": 0, "y": 0 },
-                    "sourcePosition": "Right",
-                    "targetPosition": "Left",
+                    "sourcePosition": "Position.Right",
+                    "targetPosition": "Position.Left",
                 },
                 {
                     "id": "button-2",
@@ -27,11 +33,15 @@ export const handlers: Array<HttpHandler> = [
                         "label": place_2
                     },
                     "position": { "x": 300, "y": 0 },
-                    "sourcePosition": "Right",
-                    "targetPosition": "Left",
+                    "sourcePosition": "Position.Right",
+                    "targetPosition": "Position.Left",
                 }
             ],
-            "edges": [
+            { timings, type: 'generate nodes' },
+        )
+
+        const edges = await time(
+            () => [
                 {
                     "id": "edge-button",
                     "source": "button-1",
@@ -42,7 +52,15 @@ export const handlers: Array<HttpHandler> = [
                         "targetLabel": place_2,
                     }
                 }
-            ]
-		})
-	}),
+            ],
+            { timings, type: 'generate edges' },
+        )
+
+        console.log(timings)
+
+        return json(
+            { nodes, edges },
+            { headers: { 'Server-Timing': timings.toString() } } //Create headers
+        )
+    })
 ]
